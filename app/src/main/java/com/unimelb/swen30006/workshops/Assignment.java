759143ee9046b66335c606ba324a0669da22eb89
Wu Hong Xiu <hongxiuw@student.unimelb.edu.au>
2025-03-28 10:48:54 +1100

    Create all class left in the diagram

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -0,0 +1,11 @@
+package com.unimelb.swen30006.workshops;
+
+public class Assignment {
+    private String description;
+    private String dueDate;
+    private int maxAttempts;
+    private String name;
+
+    public Submission[] invalidSubmissions() { return null; }
+    public Submission[] validSubmissions() { return null; }
+}
---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -0,0 +1,9 @@
+package com.unimelb.swen30006.workshops;
+
+public class Student extends Person {
+    private String email;
+    private String studentID;
+
+    public void sendEmailMsg(String message) {}
+    public float totalGrade() { return 0.0f;}
+}
